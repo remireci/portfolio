94f2d7825c8a61824e72ae36cbc002a579e51ee1
remireci <reci.reciproque@gmail.com>
2025-01-06 22:46:32 +0100

    fix: GA cookies

--- app/components/GoogleAnalytics.tsx
+++ app/components/GoogleAnalytics.tsx
@@ -28,7 +28,8 @@ function GoogleAnalyticsInner({ GA_MEASUREMENT_ID }: { GA_MEASUREMENT_ID: string
 
     useEffect(() => {
 
-        // console.log(pathname);
+        console.log(pathname);
+        console.log(GA_MEASUREMENT_ID);
         pageview(GA_MEASUREMENT_ID, pathname);
 
     }, [pathname, GA_MEASUREMENT_ID]);
